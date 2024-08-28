f6ef275238d31a6e198ca55a606992a561c480a2
minghung1191 <minghung1191@gmail.com>
2024-08-28 21:23:06 +0800

    create basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import StepProgress from "./components/StepProgress.jsx";
+import Step1 from "./components/Step1.jsx";
+import Step2 from "./components/Step2.jsx";
+import Step3 from "./components/Step3.jsx";
+import ProgressControl from "./components/ProgressControl.jsx"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <div>
+        <StepProgress />
+        <section>
+          <Step1 />
+          <Step2 />
+          <Step3 />
+        </section>
+        <section>
+          <ProgressControl />
+        </section>
+        </div>
+      </main>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/StepProgress.jsx
@@ -0,0 +1,33 @@
+import pgComplete from "../icons/pg-complete.svg";
+
+export default function StepProgress () {
+    return (
+      <div>
+        <section class="progress-container col col-12">
+            <span class="progress-group" data-phase="address">
+              <span class="progress-icon">
+                <span class="text">1</span>
+                <img className="icon cursor-point" src={pgComplete} alt="pg-complete.svg"/>
+              </span>
+              <span class="progress-label">寄送地址</span>
+            </span>
+            <span class="progress-bar" data-order="1"></span>
+            <span class="progress-group" data-phase="shipping">
+              <span class="progress-icon">
+                <span class="text">2</span>
+                <img className="icon cursor-point" src={pgComplete} alt="pg-complete.svg"/>
+              </span>
+              <span class="progress-label">運送方式</span>
+            </span>
+            <span class="progress-bar" data-order="2"></span>
+            <span class="progress-group" data-phase="credit-card">
+              <span class="progress-icon">
+                <span class="text">3</span>
+                <img className="icon cursor-point" src={pgComplete} alt="pg-complete.svg"/>
+              </span>
+              <span class="progress-label">付款資訊</span>
+            </span>
+        </section>
+      </div>
+    )
+}
